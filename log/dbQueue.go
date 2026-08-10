a21ba6a2daa85e766938c0f9d918e48e4f976f0c
tana <contact@o38.me>
2026-08-11 05:10:19 +0900

    メッセージの編集・削除ログ機能を追加。 (#1)
    * feat: message edit-deleate log
    
    * fix: m.Contentとcache.Contentが同一であっても編集ログが表示される問題を修正
    
    * fix: Intentsが上書きであったものを修正
    
    * fix: userの取得に失敗した際にpanicする可能性がある問題を修正
    
    * fix: fieldsのvalueに1024文字以上の文字列が入る可能性があったため切り詰め
    
    * fix: log機能をoffにしているguildでもメッセージが保存されていた問題を修正
    
    * fix: DBに直ぐ反映されず、投稿直後に削除・編集するとログが送信されない問題を修正
    
    * fix: メモリキャッシュが適切に使用されない問題を修正
    
    * fix: メモリに適切に保存されない問題を修正
    
    * fix: メモリに適切に保存されない問題を修正
    
    * fix: メモリキャッシュをDB保存時点で解放されるように変更
    
    * fix: 編集後データ保存もQueueを使用するよう変更

---
+++ log/dbQueue.go
@@ -0,0 +1,71 @@
+package msglog
+
+import (
+	"log"
+	"time"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type MsgCacheJob[T any] struct {
+	Data T
+}
+
+type DBCacheQueue[T any] struct {
+	ch      chan T
+	onFlush func([]T)
+}
+
+func NewDBCacheQueue[T any](db *gorm.DB, bufferSize int, batchSize int, onFlush func([]T)) *DBCacheQueue[T] {
+	q := &DBCacheQueue[T]{
+		ch:      make(chan T, bufferSize),
+		onFlush: onFlush,
+	}
+
+	go func() {
+		var buffer []T
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		flush := func() {
+			if len(buffer) == 0 {
+				return
+			}
+			if err := db.Clauses(clause.OnConflict{
+				UpdateAll: true,
+			}).Create(&buffer).Error; err != nil {
+				log.Println("Failed to bulk insert cache:", err)
+			} else {
+				q.onFlush(buffer)
+				buffer = buffer[:0]
+			}
+		}
+
+		for {
+			select {
+			case item, ok := <-q.ch:
+				if !ok {
+					flush()
+					return
+				}
+				buffer = append(buffer, item)
+				if len(buffer) >= batchSize {
+					flush()
+				}
+			case <-ticker.C:
+				flush()
+			}
+		}
+	}()
+
+	return q
+}
+
+func (q *DBCacheQueue[T]) Push(item T) {
+	select {
+	case q.ch <- item:
+	default:
+		log.Println("Cache queue full, dropping item")
+	}
+}
